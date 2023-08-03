d9941ca44ebeebe8b563a32a7bcb88c9afd7c6d7
Rishab Kumar <rishabkumar7@gmail.com>
2023-08-03 11:49:53 -0400

    Added homepage and changed routes for api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from typing import List
 import json
@@ -16,11 +17,9 @@ class Episode(BaseModel):
     title: str
     description: str
 
-
 class Season(BaseModel):
     season_number: int
     episodes: List[Episode]
-
 
 class CastMember(BaseModel):
     name: str
@@ -28,26 +27,32 @@ class CastMember(BaseModel):
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/seasons/", response_model=List[Season])
+# Routes
+@app.get("/", include_in_schema=False)
+async def get_homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/api/seasons/", response_model=List[Season])
 async def get_all_seasons():
     return seasons_data
 
-@app.get("/seasons/{season_number}/", response_model=Season)
+@app.get("/api/seasons/{season_number}/", response_model=Season)
 async def get_season_by_number(season_number: int):
     for season in seasons_data:
         if season["season_number"] == season_number:
             return season
     raise HTTPException(status_code=404, detail="Season not found")
 
-@app.get("/seasons/{season_number}/episodes/", response_model=List[Episode])
+@app.get("/api/seasons/{season_number}/episodes/", response_model=List[Episode])
 async def get_episodes_by_season(season_number: int):
     for season in seasons_data:
         if season["season_number"] == season_number:
             return season["episodes"]
     raise HTTPException(status_code=404, detail="Season not found")
 
-@app.get("/episodes/{episode_number}/", response_model=Episode)
+@app.get("/api/episodes/{episode_number}/", response_model=Episode)
 async def get_episode_by_number(episode_number: int):
     for season in seasons_data:
         for episode in season["episodes"]:
@@ -55,6 +60,6 @@ async def get_episode_by_number(episode_number: int):
                 return episode
     raise HTTPException(status_code=404, detail="Episode not found")
 
-@app.get("/cast/", response_model=List[CastMember])
+@app.get("/api/cast/", response_model=List[CastMember])
 async def get_cast():
     return cast_data
